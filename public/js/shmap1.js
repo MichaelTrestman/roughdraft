5f28e577b060859e8eb1d56086e9e91f751a1977
Apprentice <apprentice@master-inf-8.local>
2014-09-23 21:21:13 -0700

    init commit

---
+++ public/js/shmap1.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+
+
+
+
+
+    var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+
+    var mapOptions = {
+      zoom: 4,
+      center: myLatlng
+    }
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      map.setZoom(8);
+      map.setCenter(marker.getPosition());
+    });
+
+    var overlay = new google.maps.OverlayView();
+
+    overlay.draw = function() {};
+
+    overlay.setMap(map);
+    google.maps.event.addListener(map, "click", function (event) {
+      var latitude = event.latLng.lat();
+      var longitude = event.latLng.lng();
+      console.log( latitude + ', ' + longitude );
+    });
+
+
+
+
+
+    // google.maps.event.addListener(marker, 'mouseover', function() {
+
+    //   var projection = overlay.getProjection();
+    //   var pixel = projection.fromLatLngToContainerPixel(marker.getPosition());
+    //   // use pixel.x, pixel.y ... (after some rounding)
+
+    //   console.log(projection)
+
+    // });
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
