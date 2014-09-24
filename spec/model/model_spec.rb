814fceced1fc67d183c94edccf88cf083e5ed6eb
Michael Trestman <michael.a.trestman@gmail.com>
2014-09-24 08:10:18 -0700

    asdf

---
+++ spec/model/model_spec.rb
@@ -0,0 +1,56 @@
+describe "User" do
+  it "has a username and password for login" do
+
+  end
+  it "can create a list" do
+
+  end
+  it "can have many lists" do
+
+  end
+
+
+end
+
+describe "Collection" do
+  it "can have a new Spot added" do
+
+  end
+  it "can have a Spot removed" do
+
+  end
+
+  it "can be published to a website or as a private link" do
+
+  end
+  it "can have a lengthy description about aims or intentions" do
+
+  end
+  it "does something" do
+
+  end
+  it "can be ordered into a path" do
+
+  end
+end
+
+describe "Spot" do
+  it "can have a description" do
+
+  end
+  it "can have pictures" do
+
+  end
+  it "can have contact info" do
+
+  end
+  it "has a map location" do
+
+  end
+  it "can redirect to google map directions" do
+
+  end
+  it "can have its information updated" do
+
+  end
+end
