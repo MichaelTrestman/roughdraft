c4656967e7719b737f44f67d0c1ca1a77a22a2b5
J. Ky Marsh <jkymarsh@gmail.com>
2014-10-10 12:54:24 -0700

    Add code comments to prototypes and controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,8 @@ get '/' do
   erb :map_view
 end
 
+# don't need 'create' on this route; remember, RESTful conventions dictate that
+#  the verb 'post' tells us what we're doing: creating a spot
 post '/spots/create' do
 
   new_spot = Spot.create(
@@ -17,11 +19,13 @@ post '/spots/create' do
   {new_spot_id: new_spot.id}.to_json
 end
 
+# same comment as above; 'list' is not necessary
 get '/spots/list' do
   Spot.all.to_json
 end
 
-
+# because we're finding a spot, this should probably be a 'get,' not a 'post.' Also,
+#  shouldn't need the 'find,' use a querystring to supply search parameters
 post '/spots/find' do
 
   spot = Spot.find(params[:id].to_i)
@@ -38,7 +42,10 @@ post '/spots/find' do
   }.to_json
 end
 
+# again, don't need the 'delete' in the URL
 delete '/spots/delete' do
   Spot.destroy(params[:id].to_i)
+
+  # send back some sort of valid response!
   'yay'
 end
